b0671e8ea9b666ea2f7b839f3e3bcb17ee66aa86
lesguillemets <lesguillemets@users.noreply.github.com>
2014-08-29 19:34:15 +0900

    008.hs : using Data.List.group

--- 008.hs
+++ 008.hs
@@ -1,7 +1,10 @@
+import qualified Data.List
+
 main = do
     print $ compress "aaaabccaadeeee"
     print $ compress' "aaaabccaadeeee"
     print $ compress'' "aaaabccaadeeee"
+    print $ compress''' "aaaabccaadeeee"
 
 compress :: (Eq a) => [a] -> [a]
 compress (x0:x1:xs)
@@ -19,3 +22,6 @@ compressAcc [] acc = reverse acc
 
 compress'' [] = []
 compress'' (x:xs) = x : (compress'' $ dropWhile (==x) xs)
+
+compress''' :: (Eq a) => [a] -> [a]
+compress''' = (map head) . Data.List.group
