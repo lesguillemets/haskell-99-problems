09478944461935c74914f9124dcc26f3be9cb011
lesguillemets <lesguillemets@users.noreply.github.com>
2015-01-25 12:03:13 +0900

    28

---
+++ 28.hs
@@ -0,0 +1,24 @@
+import GHC.Exts (sortWith)
+import Data.List (sort)
+import qualified Data.Vector.Unboxed as V
+import Data.Vector.Unboxed ((!))
+
+original =  ["abc","de","fgh","de","ijkl","mn","o"]
+
+lsort' :: [[a]] -> [[a]]
+lsort' = sortWith length
+
+lfsort' :: [[a]] -> [[a]]
+lfsort' li = sortWith ((v!) . length) li
+    where
+        v = freqTable li
+
+-- freqTable :: [[a]] -> V.Vector Int
+freqTable li = let ll = sort . map length $ li
+                   lmax = 1 + last ll
+                   in
+                V.generate lmax (\n->length . takeWhile (==n) . dropWhile (/=n) $ ll)
+
+main = do
+    print $ lsort' original
+    print $ lfsort' original
